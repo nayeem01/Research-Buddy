66e2c1f805d368ffba11d0874e51dd09c3aa3b64
Nayeem Abdullah <nayeemabdullah00@gmail.com>
2024-02-03 20:58:28 +0600

    nav and side bar added

--- chrome-extension/app/layout.tsx
+++ chrome-extension/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/navbar'
+import Sidebar from './components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="night">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="drawer  lg:drawer-open">
+          <input
+            id="left-sidebar-drawer"
+            type="checkbox"
+            className="drawer-toggle"
+          />
+          {children}
+          <Sidebar />
+        </div>
+      </body>
     </html>
   )
 }
--- chrome-extension/app/page.tsx
+++ chrome-extension/app/page.tsx
@@ -2,7 +2,6 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { socketInitializer, socket } from './api/socket'
-
 interface Message {
   body: string
   sender: string
@@ -41,71 +40,73 @@ export default function Home() {
   }, [])
 
   return (
-    <main className="flex min-h-screen flex-col p-24">
-      <div className="chat chat-start">
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS chat bubble component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
-          </div>
-        </div>
-        <div className="chat-header">
-          Obi-Wan Kenobi
-          <time className="text-xs opacity-50 pl-1">12:45</time>
-        </div>
-
-        <div className="chat-bubble chat-bubble-primary">
-          What kind of nonsense is this
-        </div>
-      </div>
-
-      {/* Chat ending  */}
-
-      {mymessages.map((message, index) => (
-        <div className="chat chat-end" key={index}>
+    <div className="drawer-content flex flex-col ">
+      <main className="flex-1 overflow-y-auto md:pt-4 pt-4 px-6  bg-base-200">
+        <div className="chat chat-start">
           <div className="chat-image avatar">
             <div className="w-10 rounded-full">
-              <Image
-                alt="User avatar"
-                src="/images/bot.jpg"
-                width="64"
-                height="64"
+              <img
+                alt="Tailwind CSS chat bubble component"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
               />
             </div>
           </div>
           <div className="chat-header">
-            {message.sender}
-            <time className="text-xs opacity-50 pl-1">{message.time}</time>
+            Obi-Wan Kenobi
+            <time className="text-xs opacity-50 pl-1">12:45</time>
           </div>
-          <div className="chat-bubble chat-bubble-accent">{message.body}</div>
-          <div className="chat-footer opacity-50">Seen at 12:46</div>
-        </div>
-      ))}
 
-      {/* input */}
-      <div className="flex">
-        <input
-          type="text"
-          placeholder="Send a message"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          className="input input-bordered input-accent w-full xs"
-        />
-        <button
-          className="btn btn-outline btn-accent ml-2"
-          onClick={sendMessage}
-        >
-          Send
-          <Image
-            alt="User avatar"
-            src="/images/send.svg"
-            width="16"
-            height="16"
+          <div className="chat-bubble chat-bubble-primary">
+            What kind of nonsense is this
+          </div>
+        </div>
+
+        {/* Chat ending  */}
+
+        {mymessages.map((message, index) => (
+          <div className="chat chat-end" key={index}>
+            <div className="chat-image avatar">
+              <div className="w-10 rounded-full">
+                <Image
+                  alt="User avatar"
+                  src="/assets/bot.jpg"
+                  width="64"
+                  height="64"
+                />
+              </div>
+            </div>
+            <div className="chat-header">
+              {message.sender}
+              <time className="text-xs opacity-50 pl-1">{message.time}</time>
+            </div>
+            <div className="chat-bubble chat-bubble-accent">{message.body}</div>
+            <div className="chat-footer opacity-50">Seen at 12:46</div>
+          </div>
+        ))}
+
+        {/* input */}
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Send a message"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="input input-bordered input-accent w-full xs"
           />
-        </button>
-      </div>
-    </main>
+          <button
+            className="btn btn-outline btn-accent ml-2"
+            onClick={sendMessage}
+          >
+            Send
+            <Image
+              alt="User avatar"
+              src="/assets/send.svg"
+              width="16"
+              height="16"
+            />
+          </button>
+        </div>
+      </main>
+    </div>
   )
 }
