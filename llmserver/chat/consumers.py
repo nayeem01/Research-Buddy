8aecf32598d428c72448cdd299d8c56a151b2d0e
Nayeem Abdullah <nayeemabdullah00@gmail.com>
2024-02-08 18:26:12 +0600

    fixes

--- llmserver/chat/consumers.py
+++ llmserver/chat/consumers.py
@@ -1,35 +1,10 @@
 import json
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-<<<<<<< Updated upstream
-        self.accept()
-        # async_to_sync(self.channel_layer.group_add)("chat", self.channel_name)
-        self.send(text_data=json.dumps({"message": "You are now connected!"}))
-
-    def receive(self, text_data):
-        try:
-            text_data_json = json.loads(text_data)
-            message = text_data_json["message"]
-            self.send(text_data=json.dumps({"message": message}))
-            self.close()
-            self.close(code=4123)
-        except json.JSONDecodeError:
-            print(f"Invalid JSON: {text_data}")
-
-    def disconnect(self, close_code):
-        pass
-
-    def chat_message(self, event):
-        self.send(text_data=json.dumps(event))
-=======
         self.room_name = 'public_room'
         self.room_group_name = self.room_name
         # Join room group
@@ -63,4 +38,3 @@ class ChatConsumer(WebsocketConsumer):
         
         # Send message to WebSocket
         self.send(text_data=json.dumps({"message": message}))
->>>>>>> Stashed changes
