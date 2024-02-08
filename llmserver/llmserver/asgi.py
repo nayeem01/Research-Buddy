79038f7e3176127615067d28a856d24ab82ff264
Nayeem Abdullah <nayeemabdullah00@gmail.com>
2024-02-08 18:08:37 +0600

    realtime chat

--- llmserver/llmserver/asgi.py
+++ llmserver/llmserver/asgi.py
@@ -4,17 +4,27 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
+<<<<<<< Updated upstream
 from django.urls import path
+=======
+>>>>>>> Stashed changes
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "llmserver.settings")
 
+<<<<<<< Updated upstream
 django_asgi_app = get_asgi_application()
 
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+=======
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+>>>>>>> Stashed changes
         # websocket connection
         # WebSocket chat handler
         "websocket": URLRouter(websocket_urlpatterns),
