884f2670edf4228845757dbe77c2d14a642ddfa1
Nayeem Abdullah <nayeemabdullah00@gmail.com>
2024-02-09 10:01:27 +0600

    chat model added

--- llmserver/chat/models.py
+++ llmserver/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class UseProfile(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Message(models.Model):
+    sender_name = models.ForeignKey(
+        UseProfile, on_delete=models.CASCADE, related_name="sender"
+    )
+    receiver_name = models.ForeignKey(
+        UseProfile, on_delete=models.CASCADE, related_name="receiver"
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
